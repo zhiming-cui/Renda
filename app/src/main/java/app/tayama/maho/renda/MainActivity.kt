96c3393ae27e0796b2cb00677151a855db760cdf
maho9 <abc@example.com>
2020-03-25 16:09:46 +0900

    連打ゲーム2完成

--- app/src/main/java/app/tayama/maho/renda/MainActivity.kt
+++ app/src/main/java/app/tayama/maho/renda/MainActivity.kt
@@ -2,24 +2,67 @@ package app.tayama.maho.renda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var second=10
+
     var tapCount=0
+
+    val timer:CountDownTimer= object :CountDownTimer(10000,1000){
+
+        override fun onFinish() {
+
+            startButton.isVisible=true
+
+            tapButton.setBackgroundResource(R.drawable.background_rounded_circle_glay)
+
+            second=10
+
+            tapCount=0
+        }
+
+        override fun onTick(millisUntilFinished: Long) {
+
+            tapButton.setBackgroundResource(R.drawable.background_rounded_circle)
+
+            second=second-1
+
+            secondText.text=second.toString()
+
+
+
+
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tapButton.setOnClickListener {
-
-            tapCount=tapCount+1
+        startButton.setOnClickListener {
 
             countText.text=tapCount.toString()
 
+            startButton.isVisible=false
 
+            timer.start()
         }
+
+        tapButton.setOnClickListener {
+
+            if (second<10) {
+
+                tapCount = tapCount + 1
+
+                countText.text = tapCount.toString()
+            }
+        }
+
 
     }
 }
